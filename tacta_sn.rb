73a65e4df920e8cc09bec41c34e05631a2a405f0
nsch1 <x>
2018-02-16 18:22:54 +0100

    Add contact index & show page

--- tacta_sn.rb
+++ tacta_sn.rb
@@ -1,5 +1,18 @@
 require "sinatra"
+require "./contacts_file"
 
 get "/" do
 	"<h1>Tacta Contact Manager</h1>"
+end
+
+get "/contacts" do
+	@contacts = read_contacts
+	erb :'contacts/index'
+end
+
+get "/contacts/:index" do
+	@index = params[:index].to_i
+	contacts = read_contacts
+	@contact = contacts[@index]
+	erb :'contacts/show'
 end
